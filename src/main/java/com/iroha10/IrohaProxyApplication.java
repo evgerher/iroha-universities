97295eb0b84eda4a1fd923ed3179e4a2a13f0463
Dilschat <dilschat@yandex.ru>
2019-04-06 16:39:06 +0300

    genesis generator init

---
+++ src/main/java/com/iroha10/IrohaProxyApplication.java
@@ -0,0 +1,14 @@
+package com.iroha10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class IrohaProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrohaProxyApplication.class, args);
+	}
+
+}
